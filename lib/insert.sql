77dc1d6c6dd5b0d3aac40e973165dd478337222a
logicfirst <slee@logicfirstllc.com>
2020-11-27 12:08:45 -0500

    commit tree e7f6bcf691a62f0e62493c7069b1f68f98e0a251

---
+++ lib/insert.sql
@@ -0,0 +1,18 @@
+
+INSERT INTO bears (id, name, age, gender, color, temperament, alive) 
+VALUES (1, "Mr. Chocolate", 3, "M", "Brown", "Softy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (2, "Rowdy", 2, "M", "Brown", "Mean", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (3, "Tabitha", 5, "F", "Black", "Softy", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (4, "Sergeant Brown", 6, "M", "Brown", "Temperamental", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (5, "Melissa", 2, "F", "Black", "Mean", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (6, "Grinch", 3, "M", "Brown", "Mean", 1);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (7, "Wendy", 4, "F", "Black", "Hungry", 0);
+INSERT INTO bears (id, name, age, gender, color, temperament, alive)
+VALUES (8, NULL, 6, "M", "Brown", "Hungry", 1);
+
